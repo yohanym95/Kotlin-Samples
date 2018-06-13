5d1fe9e564aa7f1d0726ca0f111cc6d4f04a484b
yohanym95 <malshikay@gmail.com>
2018-06-13 17:43:25 +0530

    Hello World Kotlin

---
+++ Hello world_Kotlin/src/helloworld.kt
@@ -0,0 +1,9 @@
+
+//Hello World Program
+//unlike java it is not mandatory to cretae a class in every kotlin progrmas because the kotlin compiler cretates the class for us
+fun main(args : Array<String>)//main function
+{
+    println("Hello World")
+    println("Hello Kotlin")
+
+}
