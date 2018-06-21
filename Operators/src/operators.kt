bd370e023b673c3f91bd913867d779c285861a40
yohanym95 <malshikay@gmail.com>
2018-06-21 10:51:57 +0530

    Operators in Kotlin

---
+++ Operators/src/operators.kt
@@ -0,0 +1,17 @@
+fun main(args : Array<String>){
+    var num1 : Int = 3
+    var num2 : Int =5
+    var result : Int = num1+num2
+    var result1 : Int = num2-num1
+    var result2 : Int = num2 * num1
+    var result3 : Int = num2/num1
+    var result4 : Int =num2%num1
+
+    println("sum of two numbers $result")
+    println("substraction of two numbers $result1")
+    println("multiplication of two numbers $result2")
+    println("divide  of two numbers $result3")
+    println("module of two numbers $result4")
+
+
+}
