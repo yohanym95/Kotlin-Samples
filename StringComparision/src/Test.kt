7913a1b85f17f78a931f65950c614542c344654c
yohanym95 <malshikay@gmail.com>
2018-06-24 11:07:25 +0530

    added String Comparison

---
+++ StringComparision/src/Test.kt
@@ -0,0 +1,18 @@
+fun main(args : Array<String>){
+    var str1 : String = "Navin"
+    var str2 : String = "Navin"
+    var str3 : String = "navin"
+
+    if(str1.equals(str2))
+        println("same")
+    else
+        println("Not same")
+
+    if(str1.equals(str3))
+        println("same")
+    else
+        println("Not same")
+
+
+
+}
---
+++ StringComparision/src/test2.kt
@@ -0,0 +1,15 @@
+fun main(args : Array<String>){
+    var str1 : String = "Yohan"
+    var str2 : String = "Yohan"
+    var str3 : String = "yohan"
+
+    if(str1 == str2)
+        println("same")
+    else
+        println("not same")
+
+    if(str1==str3)
+        println("same")
+    else
+        println("not same")
+}
