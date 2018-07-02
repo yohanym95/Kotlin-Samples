09e334cba374d762376a9bf8256c7fe5d3b15912
yohanym95 <malshikay@gmail.com>
2018-07-02 13:59:13 +0530

    added break and continue

---
+++ #16 breakAndContinue/src/Test.kt
@@ -0,0 +1,27 @@
+fun main(args:Array<String>){
+
+    var num : Int =0
+
+    while(num<10){
+        num++
+        if(num==3){
+            break; //stop
+        }
+       println(num)
+
+
+    }
+    println()
+
+    var num1 : Int = 0
+     while (num1<10) {
+         num1++
+         if (num1 == 4){
+             continue; //skip
+         }
+
+         println(num1)
+
+     }
+
+}
