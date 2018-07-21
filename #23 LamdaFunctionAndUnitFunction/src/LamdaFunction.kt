a4b6418fa348440a7620cf2e4508ac1d50a20c4d
yohanym95 <malshikay@gmail.com>
2018-07-21 12:47:09 +0530

    added lamda and unit function

---
+++ #23 LamdaFunctionAndUnitFunction/src/LamdaFunction.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+   println("Area :"+area(4.5,6.7))
+}
+/*val function name = {
+    param_var :Type -> Operation
+     }
+
+
+     */
+val area = {
+    width : Double,
+    height:Double-> width*height
+}
+
---
+++ #23 LamdaFunctionAndUnitFunction/src/UnitFunction.kt
@@ -0,0 +1,7 @@
+fun main(args : Array<String>){
+    show()
+}
+
+fun show():Unit{//when we use unit key word we dont want use return type
+    println("hello all")
+}
